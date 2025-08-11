afdb53d1db025053393428f49285d66b8eb0682b
Sumitasawa <132041180+Sumitasawa@users.noreply.github.com>
2025-08-11 16:27:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const byteSize = (str) => {
   // write your code here
-	return str.length;
+	return new TextEncoder().encode(str).length;
 };
 
 // Do not change the code below
 const str = prompt("Enter some string.");
+
 alert(byteSize(str));
